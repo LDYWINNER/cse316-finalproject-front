8500b504b4541f1bf059f2685b1b53f81fbe3c45
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-02 17:48:36 +0900

    FIX - convert to using JWT

--- src/App.js
+++ src/App.js
@@ -1,74 +1,26 @@
-import React, { useEffect, useState } from "react";
-import Axios from 'axios';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
 
 function App() {
-  const [usernameReg, setUsernameReg] = useState("");
-  const [passwordReg, setPasswordReg] = useState("");
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [loginStatus, setLoginStatus] = useState("");
-
-  Axios.defaults.withCredentials = true;
-
-  const register = () => {
-    Axios.post("http://localhost:3001/register", {
-      username: usernameReg,
-      password: passwordReg,
-    }).then((response) => {
-      console.log(response);
-    });
-  };
-
-  const login = () => {
-    Axios.post("http://localhost:3001/login", {
-      username: username,
-      password: password,
-    }).then((response) => {
-      if (!response.data.message) {
-        setLoginStatus(response.data.message);
-      } else {
-        setLoginStatus(response.data[0].message);
-      }
-    });
-  };
-
-  useEffect(() => {
-    Axios.get("http://localhost:3001/login").then((response) => {
-      if (response.data.loggedIn == true) {
-        setRole(response.data.user[0].role);
-      }
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <div className="registration">
-        <h1>Registration</h1>
-        <label>Username</label>
-        <input type="text" onChange={(e) => {
-          setUsernameReg(e.target.value);
-        }} /><br />
-        <label>password</label>
-        <input type="text" onChange={(e) => {
-          setPasswordReg(e.target.value);
-        }} /><br />
-        <button onClick={register}>Register</button>
-      </div>
-      <div className="login">
-        <h1>Login</h1>
-        <input type="text" placeholder="Username" onChange={(e) => {
-          setUsername(e.target.value);
-        }} /><br />
-        <input type="password" placeholder="Password" onChange={(e) => {
-          setPassword(e.target.value);
-        }} />
-        <button onClick={login}>Login</button>
-      </div>
-      <h1>{loginStatus}</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/dashboard">
+          <Navbar />
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
