1aff6a32208385ab7916481e9c0f4ced8c31d91f
Minsoo <markkim1@naver.com>
2022-12-03 17:24:09 +0900

    base css file added

--- src/components/EditQuestions.js
+++ src/components/EditQuestions.js
@@ -21,8 +21,34 @@ function QuestionBox () {
     let [mode, setMode] = useState("number");
 
     return (
-        <div>
-            
+        <div class="box" style={{margin: "10px"}}>
+            <input class="input" type="text" placeholder="Text input"></input>
+            <div class="dropdown is-active">
+            <div class="dropdown-trigger">
+                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                <span>Dropdown button</span>
+                <span class="icon is-small">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+                </button>
+            </div>
+            <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                <a href="#" class="dropdown-item">
+                    Dropdown item
+                </a>
+                <a class="dropdown-item">
+                    Other dropdown item
+                </a>
+                <a href="#" class="dropdown-item is-active">
+                    Active dropdown item
+                </a>
+                <a href="#" class="dropdown-item">
+                    Other dropdown item
+                </a>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
