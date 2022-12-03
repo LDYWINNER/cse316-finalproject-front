8826dae4c6be0af8cc470d27a3357e60b31a3b46
Minsoo <markkim1@naver.com>
2022-12-04 00:36:49 +0900

    edit questions page (+) button added

--- src/components/EditQuestions.js
+++ src/components/EditQuestions.js
@@ -2,20 +2,44 @@ import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 import "../app.css";
+import SvgIcon from "@mui/material/SvgIcon";
+import { SvgIconComponent } from "@mui/icons-material";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 
 
 const EditQuestions = () => {
+    let [boxNum, setBoxNum] = useState([]);
+
+
     return (
         <div>
             <div style={{margin: "20px", backgroundColor:"transparent", display:"flex"}}>
                 <h4>Edit Questions</h4>
-                <button></button>
+                <button style={{marginLeft:"auto", backgroundColor: "transparent", border: 'transparent'}} onClick={() => {
+                    let tmp = [...boxNum];
+                    tmp.unshift(0)
+                    setBoxNum(tmp);
+                }}>
+                <AddCircleOutlineIcon sx={{ color: "black" }}></AddCircleOutlineIcon>
+
+                </button>
             </div>
-            <QuestionBox></QuestionBox>
+            <QuestionBox/>
+            {
+                boxNum.map(function(a, i){
+                    return(
+                        <QuestionBox></QuestionBox>
+                    )
+                })
+            }
+            
         </div>
     )
 }
+
+
+
 
 function QuestionBox () {
     let [modeList] = ["number", "boolean", "text", "multiple choice"];
@@ -23,34 +47,33 @@ function QuestionBox () {
     let [dropdown, setDropdown] = useState(false);
 
     return (
-        <div class="box" style={{margin: "10px"}}>
-            <input class="input" type="text" placeholder="Text input"></input>
-            
-            <div class="dropdown-trigger">
-                <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={()=>{
+        <div className="box" style={{margin: "10px"}}>
+            <input className="input" type="text" placeholder="Text input"></input>
+            <div className="dropdown-trigger">
+                <button className="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={()=>{
                     setDropdown(true);
                 }}>
                     <span>Dropdown button</span>
-                    <span class="icon is-small">
-                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    <span className="icon is-small">
+                        <i className="fas fa-angle-down" aria-hidden="true"></i>
                     </span>
                 </button>
             </div>
             {
                 dropdown ?  
-                <div class="dropdown is-active">
-                <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-                <div class="dropdown-content">
-                    <a href="#" class="dropdown-item">
+                <div className="dropdown is-active">
+                <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div className="dropdown-content">
+                    <a href="#" className="dropdown-item">
                         number
                     </a>
-                    <a href="#" class="dropdown-item">
+                    <a href="#" className="dropdown-item">
                         boolean
                     </a>
-                    <a href="#" class="dropdown-item">
+                    <a href="#" className="dropdown-item">
                         text
                     </a>
-                    <a href="#" class="dropdown-item">
+                    <a href="#" className="dropdown-item">
                         multiple choice
                     </a>
                 </div>
@@ -63,5 +86,7 @@ function QuestionBox () {
         </div>
     )
 }
+
+
 
 export default EditQuestions
