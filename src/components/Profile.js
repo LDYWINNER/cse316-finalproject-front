341f4f194677c85849f8f8c833022b8a5d1ce080
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-05 03:57:20 +0900

    CHORE

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,35 +8,35 @@ const Profile = () => {
 
 
 
-    return(
+    return (
         <div>
-            <div style={{margin: "20px", backgroundColor:"transparent", display:"flex"}}>
-                <strong style={{fontWeight:'850'}}>Edit Profile</strong>
+            <div style={{ margin: "20px", backgroundColor: "transparent", display: "flex" }}>
+                <strong style={{ fontWeight: '850' }}>Edit Profile</strong>
             </div>
-            <div className="box" style={{margin: "10px"}}>
+            <div className="box" style={{ margin: "10px" }}>
                 <strong>Profile Photo</strong>
                 <br></br>
-                <div style={{display: 'flex'}}>
-                    <div style={{margin: "auto"}}>img</div>
+                <div style={{ display: 'flex' }}>
+                    <div style={{ margin: "auto" }}>img</div>
                     {/* <input type="file" accept="image/*" style={{display: 'none'}} /> */}
-                    <button class="button is-primary" style={{margin: "auto"}}>Choose new image</button>
-                    <button style={{margin: "auto"}}>Remove image</button>
+                    <button className="button is-primary" style={{ margin: "auto" }}>Choose new image</button>
+                    <button style={{ margin: "auto" }}>Remove image</button>
                 </div>
             </div>
-            <div className="box" style={{margin: "10px"}}>
+            <div className="box" style={{ margin: "10px" }}>
                 <strong>Name</strong>
-                <input style={{marginTop: "10px"}} className="input" type="text" placeholder="Name"></input>
+                <input style={{ marginTop: "10px" }} className="input" type="text" placeholder="Name"></input>
 
             </div>
-            <div className="box" style={{margin: "10px"}}>
+            <div className="box" style={{ margin: "10px" }}>
                 <strong>Email</strong>
-                <input style={{marginTop: "10px"}} className="input" type="text" placeholder="Email"></input>
+                <input style={{ marginTop: "10px" }} className="input" type="text" placeholder="Email"></input>
 
             </div>
-            <div className="box" style={{margin: "10px"}}>
+            <div className="box" style={{ margin: "10px" }}>
                 <strong>Address</strong>
-                <input style={{marginTop: "10px"}} className="input" type="text" placeholder="Address"></input>
-                <input style={{marginTop: "10px"}} className="input" type="text" placeholder="Detail Address"></input>
+                <input style={{ marginTop: "10px" }} className="input" type="text" placeholder="Address"></input>
+                <input style={{ marginTop: "10px" }} className="input" type="text" placeholder="Detail Address"></input>
             </div>
             <br></br>
 
