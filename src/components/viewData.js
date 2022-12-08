c0743987faece4040538825e0583076838ff5468
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-12-09 00:03:30 +0900

    ADD - google chart initialization

--- src/components/viewData.js
+++ src/components/viewData.js
@@ -1,39 +1,109 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Chart from 'react-google-charts';
 import "../css/app.css";
 
 const Dashboard = () => {
     const [answers, setAnswers] = useState([]);
     const [questions, setQuestions] = useState([]);
+    const [questionsData, setQuestionsData] = useState([]);
 
-    const getAnswers = async () => {
-        const answerData = await axios.get('http://localhost:8080/answers');
-        console.log(answerData);
-        setAnswers(answerData.data);
+    const data = [
+        ["Question", "True", "False"],
+        ["2014", 1000, 400],
+    ];
+
+    const options = (title) => {
+        return (
+            {
+                chart: {
+                    title: { title },
+                }
+            }
+        )
     };
 
-    const getQuestions = async () => {
-        const questionData = await axios.get('http://localhost:8080/questions');
-        console.log(questionData);
+    const BarChart = () => {
+        return (
+            <Chart
+                chartType="Bar"
+                width="100%"
+                height="400px"
+                data={data}
+                options={options}
+            />
+        )
+    };
+
+    const LineChart = () => {
+        return (
+            <Chart
+                chartType="Line"
+                width="100%"
+                height="400px"
+                data={data}
+                options={options}
+            />
+        )
+    };
+
+    const getAnswers = useCallback(async () => {
+        const answerData = await axios.get('http://localhost:8080/answers');
+        setAnswers(answerData.data);
+    }, []);
+
+    const getQuestions = useCallback(async () => {
+        const tempQuestions = await axios.get('http://localhost:8080/questions');
+        const tempQuestionsData = tempQuestions.data;
+        setQuestionsData(tempQuestionsData);
         const temp = [];
-        for (let i = 0; i < questionData.length; i++) {
-            temp.push(questionData[i].text);
+        for (let i = 0; i < tempQuestionsData.length; i++) {
+            temp.push(tempQuestionsData[i].text);
         }
         setQuestions(temp);
-    };
+    }, []);
 
     useEffect(() => {
         getAnswers();
         getQuestions();
-    }, []);
-
-    const practiceData = [[8, 12, 2022, [12, "hello", true, "a"]]];
+    }, [getQuestions, getAnswers]);
 
     return (
         <div>
             <h2>Summary of responses organized by Question</h2>
-            <h4></h4>
+            {
+                questionsData.map((obj, i) => {
+                    console.log(questionsData[i].box_type);
+                    if (questionsData[i].box_type === "number") {
+                        //line graph
+                        return (
+                            <LineChart />
+                        );
+                    } else if (questionsData[i].box_type === "boolean") {
+                        //bar graph
+                        return (
+                            <BarChart />
+                        );
+                    } else if (questionsData[i].box_type === "multiple choice") {
+                        //bar graph
+                        return (
+                            <BarChart />
+                        );
+                    } else {
+                        //text type --> show all the answers
+                        return (
+                            <>
+                                <h2>{questionsData[i].text}</h2>
+                                {
+
+                                }
+                            </>
+                        );
+                    }
+                })
+            }
+
             <Link to="/dailyView" style={{ textDecoration: "none" }}>
                 <button>View responses per day</button>
             </Link>
